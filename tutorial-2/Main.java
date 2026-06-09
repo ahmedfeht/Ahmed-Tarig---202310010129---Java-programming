3ab72519be599226d10fe91a402a76094e956c64
ahmedfeht <tarigahmed963@gmail.com>
2026-06-09 04:48:23 +0000

    add student.java and Main.java

---
+++ tutorial-2/Main.java
@@ -0,0 +1,8 @@
+public class Main{
+    public static void main(String[] args) {
+        student s = new student("Ahmed", 23, 3.2);
+        s.displayInfo();
+        s.study();
+        s.takeExam();
+    }
+}
